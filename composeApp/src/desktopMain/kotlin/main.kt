314e0884398c44524a67d4038cff6c62e43e8c78
Mohamad Abuzaid <m.abuzaid.ali@gmail.com>
2024-06-21 16:45:46 +0300

    - Initialized Napier

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,13 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import di.initKoinDesktop
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import presentation.App
 
 fun main() = application {
+    Napier.base(DebugAntilog("Napier_Jvm"))
+
     initKoinDesktop(appComponent = JvmApplicationComponent())
 
     Window(
